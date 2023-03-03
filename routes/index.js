baa423924db4175e027da5dfb08ba9540b891e88
Alexandre <Alexandresossou@gmail.com>
2023-03-03 15:03:54 +0100

    44

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,14 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+*/
 
-
-router.get('/year', function(req, res, next) {
+router.get('/', function(req, res, next) {
   const currentYear = new Date().getFullYear();
-  res.json(`The current year is ${currentYear}.`);
+  res.json(`${currentYear}.`);
 });
 
 
